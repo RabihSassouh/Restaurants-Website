903f8bcd99a1af4a5bae5cfe069079b830791b00
Rabih Saasouh <rabih.sasouh@gmail.com>
2024-03-06 20:09:52 +0200

    working on the link between main page and singleRest page

--- scripts/singleRest.js
+++ scripts/singleRest.js
@@ -1,11 +1,10 @@
-// const favIcon = document.getElementById("favIcon");
-// const restName=document.getElementById("restName");
-// const backGroundImg=document.getElementById("backGroundImg");
+const favIcon = document.getElementById("favIcon");
+const restName=document.getElementById("restName");
 
 const container=document.querySelector(".container");
-// const description=document.getElementsById("description");
+const description=document.getElementById("description");
 let favIconStatus = false;
-let restaurant=
+let restaurant=[
     {
     id:1,
     name: "Master Chef",
@@ -18,19 +17,19 @@ let restaurant=
     phone: "Phone number: +961 70708090",
     website: "www.masterchef.lb",
     Image1: "../assets/imgs/masterChef1.jpg",
+    },
+    {
+    id:2,
+    name: "KFC",
+    description: "KFC is renowned for its iconic Southern-inspired cuisine, featuring world-famous fried"+
+    " chicken seasoned with a proprietary blend of herbs and spices.",
+    location: "Beirut, Achrafieh",
+    instagram: "KFC-lb",
+    Opening: "Opening hours: 10 am till 10 pm",
+    phone: "Phone number: +961 01202054",
+    website: "www.kfc.lb", 
     }
-    // {
-    // id:2,
-    // name: "KFC",
-    // description: "KFC is renowned for its iconic Southern-inspired cuisine, featuring world-famous fried"+
-    // " chicken seasoned with a proprietary blend of herbs and spices.",
-    // location: "Beirut, Achrafieh",
-    // instagram: "KFC-lb",
-    // Opening: "Opening hours: 10 am till 10 pm",
-    // phone: "Phone number: +961 01202054",
-    // website: "www.kfc.lb", 
-    // },
-
+]
 
 favIcon.addEventListener("click", () => {
   if (favIconStatus) {
@@ -41,14 +40,36 @@ favIcon.addEventListener("click", () => {
   favIconStatus = !favIconStatus;
 })
 
-function restaurantInfo(restaurant) {
-    restName.innerHTML=`${restaurant.name}`;
-    container.style.backgroundImage=`url("${restaurant.Image1}")`;
-    container.style.backgroundSize = "cover";
-    description.innerHTML=`${restaurant.description}`;
-}
-let i=1
-if (i==1){
-restaurantInfo(restaurant);
+function restaurantLoader(){
+    for (let i=0; i<restaurant.length;i++){
+        const restObject=restaurant[i];
+        
+        const restCard = restaurantInfo(restObject);
+    }
 }
 
+function restaurantInfo(restaurant) {
+    restName.innerHTML=`${restaurant.name}`;
+    // container.style.backgroundImage=`url("${restaurant.Image1}")`;
+    // container.style.backgroundSize = "cover";
+    // description.innerHTML=`${restaurant.description}`;
+}
+
+restCardsButtons=document.querySelectorAll(".restaurant-cards");
+console.log(restCardsButtons)
+
+// for (let i = 0; i < restCardsButtons.length; i++) {
+//     const button = restCardsButtons[i];
+
+//     button.addEventListener("click", function () {
+//       console.log(button.classList);
+//     //   button.classList.add("class");
+//     //   button.classList.remove("class");
+//     //   button.classList.toggle("class");
+//     //   button.getAttribute("product-id");
+//       // console.log(products[i]);
+//     });
+//   }
+
+
+restaurantLoader()
